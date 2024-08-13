d24c03bb832f5c31de6cb17cad8c7bdc7aa2eb6e
andrea <andreapudd@gmail.com>
2024-08-13 23:20:31 +0200

    small changes, delete some .o

--- libft_vector/vector_gen/constants.py
+++ libft_vector/vector_gen/constants.py
@@ -1,2 +1,2 @@
 USER42 = "apuddu"
-types = {"vi":"int", "vl":"long", "vlu":"long unsigned int", "vstr":"char *"}
+types = {"vi":"int", "vl":"long", "vlu":"long unsigned int", "vstr":"char *"}#, "vvec":"t_vec3", "grid":"t_vvec *"}
