49d06c98ae4b288bf5b5866d4c318c6d105005b3
andrea <andreapudd@gmail.com>
2025-03-03 11:50:02 +0100

    move miniRT to other repo, small changes to stuff

--- libft_vector/vector_gen/constants.py
+++ libft_vector/vector_gen/constants.py
@@ -1,2 +1,4 @@
 USER42 = "apuddu"
-types = {"vi":"int", "vl":"long", "vlu":"long unsigned int", "vstr":"char *", "vec":"void *", "vch":"char"}
+# types = {"vi":"int", "vl":"long", "vlu":"long unsigned int", "vstr":"char *", "vec":"void *", "vch":"char"}
+
+types = {"vshape":"t_shape"}
